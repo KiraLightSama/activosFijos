3a00bdfc63ba2f480a5319b33973ea3c5f9f5aa9
solcita <sol_angeluz_@hotmail.com>
2017-10-09 20:40:24 -0400

    bitacora

---
+++ src/app/Bitacora.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bitacora extends Model
+{
+
+    protected $table ='Bitacora';
+    protected $primarykey ='id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id ', 'Correo','Nombre','Accion','Navegador','fecha'
+    ];
+
+}
